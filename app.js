88bb6f3c430b31b384c413d0d41658c33ccc8cff
deepak <deepak163mandal@gmail.com>
2024-09-04 01:42:27 +0530

    make header responsive and add java script for responsive

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-javascript
+const bar = document.getElementById('bar');
+const nav = document.getElementById('nav-bar');
+const close = document.getElementById('close');
+
+if(bar){
+    bar.addEventListener('click', () =>{
+        nav.classList.add('active'); 
+    })
+}
+if(close){
+    close.addEventListener('click', () =>{
+        nav.classList.remove('active');
+    })
+}
